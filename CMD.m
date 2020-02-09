77cb5e994206d64bba8c7b451fb40991312bd5e7
BenjaminGutman <60733209+BenjaminGutman@users.noreply.github.com>
2020-02-09 15:52:02 +0200

    Add files via upload

--- CMD.m
+++ CMD.m
@@ -3,18 +3,21 @@
 % notation: Solve ||PI-M||^2
 % Equivalent to solving P* D^2 P I=P* D^2 M 
 
-function I = CMD(Measured, Kernels, I, tol, MaxIter, MinIter, Lambda, sigmas)
+function I = CMD(Measured, Kernels, I, tol, MaxIter, MinIter, Lambda, sigmas, enableGPUflag)
 
     % normilize PSF
     for i=1:length(Measured)
-        Kernels{i} = gpuArray(Kernels{i}./sum(Kernels{i}(:))); 
+        Kernels{i} = Kernels{i}./sum(Kernels{i}(:));
+        if enableGPUflag
+            Kernels{i} = gpuArray(Kernels{i}); 
+        end
     end
     
     % start by computing P*M
-    adjPM = AdjointOperator(Measured, Kernels, sigmas);  %%adding the images together 
+    adjPM = AdjointOperator(Measured, Kernels, sigmas, enableGPUflag);  %%adding the images together 
 
     % Solve P*PI=P*M using conjugate gradients
-    temp = SquareOperator(I, Kernels, Lambda, sigmas);
+    temp = SquareOperator(I, Kernels, Lambda, sigmas, enableGPUflag);
     r = adjPM - temp;
     p = r;
     k = 0;
@@ -23,7 +26,7 @@ function I = CMD(Measured, Kernels, I, tol, MaxIter, MinIter, Lambda, sigmas)
 
     % CG iterations
     while (err>tol) && (k<MaxIter) || (k<MinIter)
-        PadjPp = SquareOperator(p, Kernels, Lambda, sigmas);
+        PadjPp = SquareOperator(p, Kernels, Lambda, sigmas, enableGPUflag);
         alpha = sum2(r.^2)/sum2(p.*PadjPp);
         I = I + alpha*p;
         r_next = r - alpha*PadjPp;
@@ -53,18 +56,21 @@ function Outputs = DirectOperator(Image, Kernels)
 end
 
 % Compute P* D^2 X
-function Output = AdjointOperator(Images, Kernels, sigmas)
+function Output = AdjointOperator(Images, Kernels, sigmas, enableGPUflag)
     N = length(Kernels);
-    Output = gpuArray(conv2(Images{1}, zeros(size(Kernels{1}), 'single'), 'full'));
+    Output = conv2(Images{1}, zeros(size(Kernels{1}), 'single'), 'full');
+    if enableGPUflag
+        Output = gpuArray(Output);
+    end
     for i = 1:N
         Output = Output + conv2(Images{i}, rot90((Kernels{i}),2), 'full')/(2*sigmas(i)^2);
     end
 end
 
 % Compute P* D^2 PX
-function Output = SquareOperator(I, Kernels, Lambda, sigmas)
+function Output = SquareOperator(I, Kernels, Lambda, sigmas, enableGPUflag)
     Output = AdjointOperator(DirectOperator(I, Kernels), Kernels, ...
-        sigmas) + Lambda*I;
+        sigmas, enableGPUflag) + Lambda*I;
 end
     
 
--- run.m
+++ run.m
@@ -16,6 +16,8 @@ options.separation_init = 4;                              % used in 'separation_
 options.flag.SR_alone = 1;
 options.flag.CMD_alone = 1;
 options.flag.srSAXS = 1;
+options.flag.plot = 1;
+options.flag.enableGPU = 0;
 
 options.PSF.number = size(options.PSF.params, 1)^2;
 options.BeamStopSz = min(options.CirclesParams(:, 1))-3;  % smallest circle radius - 5 pixel
@@ -35,31 +37,35 @@ LRImages = Decimation(corrupted, options);                % decimation for low r
 %% SR
 % SR as first step of srSAXS
 if options.flag.srSAXS            
-    
     HR_for_CMD = cell(1, options.LRfactor);
+    f = waitbar(0, 'SR in progress');
     
     for i = 1:options.PSF.number
+        f = waitbar(i/options.PSF.number, f ,'SR in progress');
         HR_for_CMD{i} = my_SR(squeeze(LRImages(:, :, :, i)), options.LRfactor, 1);
     end
+    close(f)
     
 end
 
 % SR alone
-if options.flag.SR_alone  
-    
+if any([options.flag.SR_alone  options.flag.srSAXS])  
+    f = waitbar(0, 'SR in progress');
     HR_alone = cell(1, options.LRfactor);
     I_SR_alone = HR_alone;
     q_SR_alone = HR_alone;
     deltaSRalone = zeros(1, options.LRfactor);
     
     for i = 1:options.PSF.number
+        f = waitbar(i/options.PSF.number, f ,'SR in progress');
         HR_alone{i} = my_SR(squeeze(LRImages(:, :, :, i)), ...
             options.LRfactor, PSF{i});
         [I_SR_alone{i}, q_SR_alone{i}, ~] = ...
             CircularIntegration(HR_alone{i});                     % circular int. of reconstruction
         deltaSRalone(i) = separation_criterion(q_SR_alone{i}, ...
-            I_SR_alone{i}, options.separation_init);           % delta of reconstruction
+            I_SR_alone{i}, options.separation_init/options.LRfactor);           % delta of reconstruction
     end
+    close(f)
     
 end
 
@@ -80,9 +86,12 @@ if options.flag.CMD_alone
     sigmas = normpdf(flux, mean(flux)*S, mean(flux)/3);
     sigmas = sigmas./max(sigmas);                             % normalization of sigma
 
-    CMD_alone_Rec = CMD(corrupted, PSF, I0, eps, 200, 0, lambda,1./sigmas); % main algorithm
+    CMD_alone_Rec = CMD(corrupted, PSF, I0, eps, 200, 0, lambda,1./sigmas, options.flag.enableGPU); % main algorithm
 
-    CMD_alone_Rec = gather(CMD_alone_Rec); CMD_alone_Rec(CMD_alone_Rec<0)=0;                          % reconstruction final adjustment
+	CMD_alone_Rec(CMD_alone_Rec<0)=0;
+    if options.flag.enableGPU
+        CMD_alone_Rec = gather(CMD_alone_Rec);                           % reconstruction final adjustment
+    end
     [I_CMD_alone_Rec, q_CMD_alone_Rec, ~] = CircularIntegration(CMD_alone_Rec);                     % circular int. of reconstruction
     deltaCMDalone = separation_criterion(q_CMD_alone_Rec, ...
         I_CMD_alone_Rec, options.separation_init);            % delta of reconstruction
@@ -103,21 +112,31 @@ if options.flag.srSAXS
     sigmas = normpdf(flux, mean(flux)*S, mean(flux)/3);
     sigmas = sigmas./max(sigmas);                             % normalization of sigma
 
-    srSAXS_Rec = CMD(HR_for_CMD, PSF, I0, eps, 200, 0, lambda,1./sigmas); % main algorithm
-
-    srSAXS_Rec = gather(srSAXS_Rec); srSAXS_Rec(srSAXS_Rec<0)=0;                          % reconstruction final adjustment
+    srSAXS_Rec = CMD(HR_for_CMD, PSF, I0, eps, 200, 0, lambda,1./sigmas, options.flag.enableGPU); % main algorithm
+	srSAXS_Rec(srSAXS_Rec<0)=0;
+    if options.flag.enableGPU
+        srSAXS_Rec = gather(srSAXS_Rec);                           % reconstruction final adjustment
+    end
     [I_srSAXS, q_srSAXS, ~] = CircularIntegration(srSAXS_Rec);                % circular int. of reconstruction
     deltasrSAXS = separation_criterion(q_srSAXS, I_srSAXS, options.separation_init); % delta of reconstruction
 end
 %% plot 
-figure('WindowStyle', 'docked', 'NumberTitle', 'off', 'Name', 'results')
-hold on; grid on; grid minor
-plot(qGT, IGT./max(IGT), 'DisplayName', sprintf('GT  %.2f', deltaGT))
-plot(q_CMD_alone_Rec, I_CMD_alone_Rec./max(I_CMD_alone_Rec), ...
-    'DisplayName', sprintf('CMD reco  %.2f', deltaCMDalone))
-plot(q_srSAXS, I_srSAXS./max(I_srSAXS), ...
-    'DisplayName', sprintf('srSAXS reco  %.2f', deltasrSAXS))
-plot(q_SR_alone{1}, I_SR_alone{1}./max(I_SR_alone{1}), ...
-    'DisplayName', sprintf('SR reco  %.2f', deltaSRalone{1}))
-l = legend;
-l.Title.String = 'numbers for delta critiria';
+if options.flag.plot
+    figure('WindowStyle', 'docked', 'NumberTitle', 'off', 'Name', 'results')
+    hold on; grid on; grid minor
+    plot(qGT, IGT./max(IGT), 'DisplayName', sprintf('GT  %.2f', deltaGT))
+        if options.flag.CMD_alone
+            plot(q_CMD_alone_Rec, I_CMD_alone_Rec./max(I_CMD_alone_Rec), ...
+                'DisplayName', sprintf('CMD reco  %.2f', deltaCMDalone))
+        end
+        if options.flag.srSAXS
+            plot(q_srSAXS, I_srSAXS./max(I_srSAXS), ...
+                'DisplayName', sprintf('srSAXS reco  %.2f', deltasrSAXS))
+        end
+        if options.flag.SR_alone
+            plot(q_SR_alone{1}, I_SR_alone{1}./max(I_SR_alone{1}), ...
+                'DisplayName', sprintf('SR reco  %.2f', deltaSRalone(1)))
+        end
+    l = legend;
+    l.Title.String = 'numbers for delta critiria';
+end
